1f1db15f9e89398ad15627d331648435b8346e80
Fredrik Rødland <fmr@mac_maisen.(none)>
2011-10-07 13:41:26 +0200

    also supports : and = in keys

--- commands.py
+++ commands.py
@@ -75,7 +75,7 @@ def execute(**kargs):
     strings = readMessagesFile(messagesFile)
     stringsToAdd = []
     for string in i18nStrings:
-      string = string.replace(" ", "\\ ")
+      string = string.replace(" ", "\\ ").replace(":", "\\:").replace("=", "\\=")
       if string not in strings:
         stringsToAdd.append(string)
     appendToMessagesFile(messagesFile, stringsToAdd)
@@ -111,7 +111,7 @@ def appendToMessagesFile(path, strings):
   if len(strings) > 0:
     text = "\n\n# Next %i lines were automatically added by i18n-extract on %s\n" % (len(strings), time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
     for string in strings:
-      text += "%s = %s %s\n" % (string, "TODO", string.replace("\\ ", " "))
+      text += "%s = %s %s\n" % (string, "TODO", string.replace("\\ ", " ").replace("\\:", ":").replace("\\=", "="))
     f = open(path, "a")
     f.write(text)
     f.close()
